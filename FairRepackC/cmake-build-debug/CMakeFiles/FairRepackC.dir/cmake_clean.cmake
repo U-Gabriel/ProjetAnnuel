952b6cf48b27f23455186ae19791b45626bd7b54
Elesdes <elesdes404@gmail.com>
2021-04-14 21:55:21 +0200

    FAIRREPACK

---
+++ FairRepackC/cmake-build-debug/CMakeFiles/FairRepackC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FairRepackC.dir/main.c.obj"
+  "FairRepackC.exe"
+  "FairRepackC.exe.manifest"
+  "FairRepackC.pdb"
+  "libFairRepackC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FairRepackC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
